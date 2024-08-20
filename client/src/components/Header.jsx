3414f403b26bc467fa826d6b38812c3e1b0012df
chethan <chethanmcssvn@gmail.com>
2024-08-20 19:42:41 +0530

    create signup api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-violet-400 shadow-md'>
+    <header className='bg-orange-400 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-blue-200'>Nandi</span>
-            <span className='text-red-500'>Enclave</span>
+            <span className='text-red-800'>Nandi</span>
+            <span className='text-green-700'>Enclave</span>
         </h1>
         </Link>
         <form className='bg-slate-200 p-3 rounded-lg flex items-center'>
@@ -18,13 +18,13 @@ export default function Header() {
         </form>
         <ul className='flex gap-5'>
             <Link to='/'>
-            <li className='hidden sm:inline text-green-800 hover:underline'>Home</li>
+            <li className='hidden sm:inline text-blue-500 hover:underline'>Home</li>
             </Link>
             <Link to='/about'>
-            <li className='hidden sm:inline text-green-800 hover:underline'>About</li>
+            <li className='hidden sm:inline text-blue-500 hover:underline'>About</li>
             </Link>
             <Link to='/signin'>
-            <li className=' text-green-800 hover:underline'>Sign in</li>
+            <li className=' text-blue-500 hover:underline'>Sign in</li>
             </Link>
         </ul>
         </div>
