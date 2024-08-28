0468dad142e27aff014afb17a284e7c9f746e462
chethan <chethanmcssvn@gmail.com>
2024-08-28 21:08:49 +0530

    complete upload img fun

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@ import Home from './pages/Home';
 import About from './pages/about';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import Profile from './pages/profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import Profile from './pages/Profile';
 
 export default function App() {
   return ( 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,119 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
+  const fileRef = useRef(null);
+  const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUpLoadError, setFileUpLoadError] = useState(false);
+  const [formData, setFormData] = useState({});
+ 
+
+  // firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+  useEffect(() => {
+    if(file) {
+      handleFileUpload();
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUpLoadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then
+        ((downloadURL) =>
+          setFormData({ ...FormData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
   return (
-    <div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-3"
+        />
+        <p>
+          { fileUpLoadError ?
+          (<span className="text-blue-800"> error image upload
+          </span>)  :
+        filePerc > 0 &&  filePerc < 100 ? (
+          <span className="text-green-700">
+            {`uploading ${filePerc}%`}
+          </span>) :
+          
+          filePerc === 100 ? (
+            <span className="text-red-700">successfully uploaded!</span> ) 
+            :
+            (""
+          )}
+        </p>
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg"
+        />
+        <button
+          className="bg-red-500 text-white rounded-lg p-3 uppercase
+        hover:opacity-75 disabled:opacity-65"
+        >
+          update
+        </button>
+      </form>
+      <div className="flex justify-between mt-4">
+        <span className="text-green-600 cursor-pointer">Delete Account</span>
+        <span className="text-green-600 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 }
